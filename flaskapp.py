46c35f3f3e1a0b7f63b0f704aa6df05192e4c6f8
frohenk <frohenk@gmail.com>
2015-12-08 14:31:32 +0900

    Some checks

--- flaskapp.py
+++ flaskapp.py
@@ -1,37 +1,44 @@
 import os
 from datetime import datetime
-from mysqlclient import curs,cnx
+from mysqlclient import curs, cnx
 from flask import Flask, request, flash, url_for, redirect, \
     render_template, abort, send_from_directory
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
+
 @app.route('/')
 def index():
-    return render_template('schedule.html',schedule_active=True)
+    return render_template('schedule.html', schedule_active=True)
+
+
 @app.route('/subjects/')
 def subjects_page():
-    return render_template('subjects.html',subjects_active=True)
+    return render_template('subjects.html', subjects_active=True)
+
+
 @app.route('/project/')
 def about_project():
-    return render_template('project.html',project_active=True)
+    return render_template('project.html', project_active=True)
+
 
 #
 
 @app.route('/admins')
 def get_admins():
-    res="";
+    res = ""
     curs.execute("SELECT * FROM admins")
 
-    for (id,username,password) in curs:
-        res+="%s\n"%username.decode("utf8")
+    for (id, username, password) in curs:
+        res += "%s\n" % username.decode("utf8")
     return res
+
 
 @app.route('/<path:resource>')
 def serve_static_resource(resource):
     return send_from_directory('static/', resource)
 
+
 if __name__ == '__main__':
     app.run()
-
