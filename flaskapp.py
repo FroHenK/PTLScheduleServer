41302c05f470dbae742a0da1699820c8ad12af3b
frohenk <frohenk@gmail.com>
2015-12-09 12:58:11 +0900

    Some checks

--- flaskapp.py
+++ flaskapp.py
@@ -1,29 +1,63 @@
 import os
+import hashlib
 from datetime import datetime
-from mysqlclient import curs, cnx
-from flask import Flask, request, flash, url_for, redirect, \
+from mysqlclient import curs, cnx, get_grades, get_subjects, get_homeworks, is_valid_admin, add_homework_db
+from flask import Flask, request, session, flash, url_for, redirect, \
     render_template, abort, send_from_directory
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
 
 
+def template_imports():
+    res = {}
+    res["grades_array"] = get_grades()
+    res["subjects_array"] = get_subjects()
+    return res
+
+
+@app.route('/set_grade/', methods=['POST'])
+def set_grade():
+    session['grade_selected_id'] = int(request.form['grade_id'])
+    return redirect('/')
+
+
+@app.route('/login/', methods=['POST'])
+def admin_login():
+    s = str(hashlib.md5(str(request.form['password']).encode('utf8')).hexdigest())
+    if is_valid_admin(str(request.form['username']), s):
+        session['is_admin'] = True
+    return redirect('/')
+
+
 @app.route('/')
 def index():
-    return render_template('schedule.html', schedule_active=True)
+    return render_template('schedule.html', schedule_active=True, **template_imports())
 
 
 @app.route('/subjects/')
 def subjects_page():
-    return render_template('subjects.html', subjects_active=True)
+    return render_template('subjects.html', subjects_active=True, **template_imports())
+
+
+@app.route('/add_homework/<int:grade_id>/<int:subject_id>/', methods=["POST"])
+def on_add_homework(grade_id, subject_id):
+    add_homework_db(grade_id,subject_id,str(request.form['data']))
+
+    return redirect('/subjects/%s' % subject_id)
+
+
+@app.route('/subjects/<int:subject_id>/')
+def draw_subject(subject_id):
+    return render_template('homeworks.html', subjects_active=True, subject_id=subject_id,
+                           homeworks_array=get_homeworks(session['grade_selected_id'], subject_id),
+                           **template_imports())
 
 
 @app.route('/project/')
 def about_project():
-    return render_template('project.html', project_active=True)
+    return render_template('project.html', project_active=True, **template_imports())
 
-
-#
 
 @app.route('/admins')
 def get_admins():
--- mysqlclient.py
+++ mysqlclient.py
@@ -1,6 +1,28 @@
 import os
 import mysql
+
 from mysql.connector import (connection)
+
+
+class Grade:
+    def __init__(self, id, title):
+        self.title = title
+        self.id = id
+
+
+class Homework:
+    def __init__(self, id, grade_id, subject_id, data):
+        self.subject_id = subject_id
+        self.grade_id = grade_id
+        self.data = data
+        self.id = id
+
+
+class Subject:
+    def __init__(self, id, title):
+        self.title = title
+        self.id = id
+
 
 MYSQL_HOST = os.environ.get('OPENSHIFT_MYSQL_DB_HOST', '127.0.0.1')
 MYSQL_USER = os.environ.get('OPENSHIFT_MYSQL_DB_USERNAME', 'root')
@@ -10,9 +32,58 @@ MYSQL_DB = 'ptl'
 
 cnx = connection.MySQLConnection(user=MYSQL_USER, password=MYSQL_PASSWORD,
                                  host=MYSQL_HOST,
-                                 database=MYSQL_DB,charset='utf8')
+                                 database=MYSQL_DB, charset='utf8')
 
-curs = cnx.cursor()
+curs = cnx.cursor(buffered=True)
+
+
+def get_grades():
+    curs.execute('SELECT * FROM grade')
+    res = curs.fetchall()
+
+    grades_array = []
+    for (id, title) in res:
+        grades_array.append(Grade(int(id), str(title.decode("utf8"))))
+    return grades_array
+
+
+def get_homeworks(grade_id2, subject_id2):
+    id_ = 'SELECT * FROM homework WHERE grade_id=%s AND subject_id=%s ORDER BY id DESC' % (grade_id2, subject_id2)
+
+    curs.execute(id_)
+
+    res = curs.fetchall()
+
+    homeworks_array = []
+    for (id, grade_id, subject_id, data) in res:
+        homeworks_array.append(Homework(int(id), grade_id, subject_id, str(data.decode("utf8"))))
+    return homeworks_array
+
+
+def is_valid_admin(username, password_md5):
+    id_ = 'SELECT * FROM admins WHERE username=\'%s\' AND password=\'%s\'' % (username, password_md5)
+    print(id_)
+    curs.execute(id_)
+    if not curs.fetchone()[0]:
+        return False
+    return True
+
+
+def add_homework_db(grade_id, subject_id, data):
+    insert_command = 'INSERT INTO homework (grade_id, subject_id, data) VALUES (%s,%s,\'%s\')' % (
+    grade_id, subject_id, data)
+    curs.execute(insert_command)
+    cnx.commit()
+
+
+def get_subjects():
+    curs.execute('SELECT * FROM subject')
+    res = curs.fetchall()
+
+    subjects_array = []
+    for (id, title) in res:
+        subjects_array.append(Subject(int(id), str(title.decode("utf8"))))
+    return subjects_array
 
 
 def close_connection():
