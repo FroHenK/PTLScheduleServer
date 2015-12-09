cbf97f49e5a70b4c37abdfb22f9797bb89b7aba8
frohenk <frohenk@gmail.com>
2015-12-09 17:33:16 +0900

    Some checks

--- flaskapp.py
+++ flaskapp.py
@@ -1,18 +1,25 @@
 import os
 import hashlib
-from datetime import datetime
-from mysqlclient import curs, cnx, get_grades, get_subjects, get_homeworks, is_valid_admin, add_homework_db
+import json
+from datetime import datetime, timedelta
+
+from mysqlclient import curs, cnx, get_grades, get_subjects, get_homeworks, is_valid_admin, add_homework_db, \
+    create_day_element_db, get_day_db, get_day_db_id, get_homework_id, delete_day_db_id, delete_homework_id,get_all_day_db,get_all_homeworks
 from flask import Flask, request, session, flash, url_for, redirect, \
     render_template, abort, send_from_directory
 
 app = Flask(__name__)
+
 app.config.from_pyfile('flaskapp.cfg')
 
 
 def template_imports():
     res = {}
+
     res["grades_array"] = get_grades()
+
     res["subjects_array"] = get_subjects()
+    res["timedelta"] = timedelta
     return res
 
 
@@ -20,6 +27,46 @@ def template_imports():
 def set_grade():
     session['grade_selected_id'] = int(request.form['grade_id'])
     return redirect('/')
+
+
+@app.route("/create_day_element/<string:usr_date>/", methods=['POST'])
+def create_day_element(usr_date):
+    time_str = request.form['usr_time']
+    subject_id = int(request.form['subject_id'])
+    if len(time_str) == 0:
+        time_str = '00:00'
+    current_datetime = datetime.strptime(usr_date + ' ' + time_str, '%Y-%m-%d %H:%M')
+    create_day_element_db(session['grade_selected_id'], current_datetime, subject_id)
+    return redirect('/get_day/' + usr_date + "/")
+
+
+@app.before_first_request
+def b_request():
+    session['grade_selected_id'] = 105  # FIXME  bad code
+
+
+@app.route("/get_day/<string:usr_date>/", methods=['GET'])
+def get_homework(usr_date):
+    current_datetime = datetime.strptime(usr_date, '%Y-%m-%d')
+
+    day_db = get_day_db(session['grade_selected_id'], current_datetime)
+    return render_template('schedule.html', day_elements_array=day_db, schedule_active=True,
+                           current_datetime=current_datetime,
+                           **template_imports())
+
+
+@app.route("/delete_day_element/<int:id>")
+def delete_day_elem(id):
+    day_element = get_day_db_id(id)
+    delete_day_db_id(id)
+    return redirect('/get_day/' + day_element.date.strftime('%Y-%m-%d'))
+
+
+@app.route("/delete_homework/<int:id>")
+def delete_homework(id):
+    day_element = get_homework_id(id)
+    delete_homework_id(id)
+    return redirect('/subjects/' + str(day_element.subject_id))
 
 
 @app.route('/login/', methods=['POST'])
@@ -32,7 +79,10 @@ def admin_login():
 
 @app.route('/')
 def index():
-    return render_template('schedule.html', schedule_active=True, **template_imports())
+    # TODO make it
+    current_datetime = datetime.now()
+
+    return get_homework(current_datetime.strftime("%Y-%m-%d"))
 
 
 @app.route('/subjects/')
@@ -42,7 +92,7 @@ def subjects_page():
 
 @app.route('/add_homework/<int:grade_id>/<int:subject_id>/', methods=["POST"])
 def on_add_homework(grade_id, subject_id):
-    add_homework_db(grade_id,subject_id,str(request.form['data']))
+    add_homework_db(grade_id, subject_id, str(request.form['data']))
 
     return redirect('/subjects/%s' % subject_id)
 
@@ -72,6 +122,23 @@ def get_admins():
 @app.route('/<path:resource>')
 def serve_static_resource(resource):
     return send_from_directory('static/', resource)
+
+
+def encode_b(obj):
+
+    return obj.__dict__
+
+
+@app.route("/get_json/")
+def json_api():
+    json_data = {}
+    json_data['grades'] = get_grades()
+    json_data['subjects'] = get_subjects()
+    json_data['homeworks'] = get_all_homeworks()
+    json_data['day_elements'] = get_all_day_db()
+
+
+    return json.dumps(json_data, default=encode_b)
 
 
 if __name__ == '__main__':
--- mysqlclient.py
+++ mysqlclient.py
@@ -1,12 +1,21 @@
 import os
 import mysql
-
+from datetime import datetime
 from mysql.connector import (connection)
 
 
 class Grade:
     def __init__(self, id, title):
         self.title = title
+        self.id = id
+
+
+class DayElement:
+    def __init__(self,id, grade_id, date, subject_id,subject_name):
+        self.subject_name = subject_name
+        self.subject_id = subject_id
+        self.date = date
+        self.grade_id = grade_id
         self.id = id
 
 
@@ -47,6 +56,7 @@ def get_grades():
     return grades_array
 
 
+
 def get_homeworks(grade_id2, subject_id2):
     id_ = 'SELECT * FROM homework WHERE grade_id=%s AND subject_id=%s ORDER BY id DESC' % (grade_id2, subject_id2)
 
@@ -58,6 +68,24 @@ def get_homeworks(grade_id2, subject_id2):
     for (id, grade_id, subject_id, data) in res:
         homeworks_array.append(Homework(int(id), grade_id, subject_id, str(data.decode("utf8"))))
     return homeworks_array
+
+def get_all_homeworks():
+    id_ = 'SELECT * FROM homework'
+
+    curs.execute(id_)
+
+    res = curs.fetchall()
+
+    homeworks_array = []
+    for (id, grade_id, subject_id, data) in res:
+        homeworks_array.append(Homework(int(id), grade_id, subject_id, str(data.decode("utf8"))))
+    return homeworks_array
+
+def create_day_element_db(grade_id, datetime, subject_id):
+    insert_command = 'INSERT INTO day_element (grade_id, date, subject_id)  VALUES (%s,\'%s\',%s)' % (
+        grade_id, datetime.strftime('%Y-%m-%d %H:%M:00'), subject_id)
+    curs.execute(insert_command)
+    cnx.commit()
 
 
 def is_valid_admin(username, password_md5):
@@ -71,9 +99,71 @@ def is_valid_admin(username, password_md5):
 
 def add_homework_db(grade_id, subject_id, data):
     insert_command = 'INSERT INTO homework (grade_id, subject_id, data) VALUES (%s,%s,\'%s\')' % (
-    grade_id, subject_id, data)
+        grade_id, subject_id, data)
     curs.execute(insert_command)
     cnx.commit()
+
+def get_day_db(grade_id,date):
+    elements_array=[]
+    command='SELECT * FROM day_element WHERE grade_id = %s AND date BETWEEN \'%s 00:00:00\' AND \'%s 23:59:59\' ORDER BY date'%(grade_id,date.strftime('%Y-%m-%d'),date.strftime('%Y-%m-%d'))
+    curs.execute(command)
+    res = curs.fetchall()
+    for (id, grade_id, date, subject_id) in res:
+            curs.execute('SELECT title FROM subject WHERE id=%s'%subject_id)
+            title=curs.fetchone()[0]
+            elements_array.append( DayElement(id,grade_id,date,subject_id,str(title.decode('utf8'))) )
+
+    return elements_array
+
+def get_all_day_db():
+    elements_array=[]
+    command='SELECT * FROM day_element'
+    curs.execute(command)
+    res = curs.fetchall()
+    for (id, grade_id, date, subject_id) in res:
+            curs.execute('SELECT title FROM subject WHERE id=%s'%subject_id)
+            title=curs.fetchone()[0]
+            elements_array.append( DayElement(id,grade_id,date,subject_id,str(title.decode('utf8'))) )
+
+    return elements_array
+
+def get_homework_id(id):
+    id_ = 'SELECT * FROM homework WHERE id=%s' % (id)
+
+    curs.execute(id_)
+
+    res = curs.fetchall()
+
+    homeworks_array = []
+    for (id, grade_id, subject_id, data) in res:
+        homeworks_array.append(Homework(int(id), grade_id, subject_id, str(data.decode("utf8"))))
+    return homeworks_array[0]
+
+def delete_day_db_id(id):
+
+    command='DELETE  FROM day_element WHERE id=%s'%id
+
+    curs.execute(command)
+    cnx.commit()
+
+def delete_homework_id(id):
+
+    command='DELETE  FROM homework WHERE id=%s'%id
+
+    curs.execute(command)
+    cnx.commit()
+
+def get_day_db_id(id):
+    elements_array=[]
+    command='SELECT * FROM day_element WHERE id=%s'%id
+    curs.execute(command)
+    res = curs.fetchall()
+    for (id, grade_id, date, subject_id) in res:
+            curs.execute('SELECT title FROM subject WHERE id=%s'%subject_id)
+            title=curs.fetchone()[0]
+            elements_array.append( DayElement(id,grade_id,date,subject_id,str(title.decode('utf8'))) )
+
+    return elements_array[0]
 
 
 def get_subjects():
@@ -84,6 +174,7 @@ def get_subjects():
     for (id, title) in res:
         subjects_array.append(Subject(int(id), str(title.decode("utf8"))))
     return subjects_array
+
 
 
 def close_connection():
